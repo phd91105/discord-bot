ff51ae745595e7d5e6dc765d1f420d4db4e8474a
Pham Duy <phd051199@gmail.com>
2023-08-24 19:59:34 +0700

    rm dist

--- dist/commands/index.js
+++
@@ -1,6 +0,0 @@
-import { cherryPick } from './cherryPick.js';
-import { getPublicIP } from './publicIP.js';
-export const bootstrap = (client) => {
-    cherryPick(client);
-    getPublicIP(client);
-};
